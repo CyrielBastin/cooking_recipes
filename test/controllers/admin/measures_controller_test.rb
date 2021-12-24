e53f43c374e7be436e93e94bcffbdf11c690e44f
Grimm <cyriel.bastin@zoho.com>
2021-12-24 15:10:45 +0100

    add remaining controller tests

---
+++ test/controllers/admin/measures_controller_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class Admin::MeasuresControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    default_url_options[:host] = 'en.application.local'
+    @measure = measures :teaspoon
+  end
+
+  test 'should get index' do
+    get admin_measures_url
+
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_admin_measure_url
+
+    assert_response :success
+  end
+
+  test 'should create measure _en' do
+    assert_difference('Measure.count') do
+      post admin_measures_url, params:
+        { measure:
+            { name: 'Test Measure' } }
+    end
+
+    assert_equal 'Your Measure was successfully created !', flash[:success]
+    measure = Measure.last
+    assert_equal 'Test Measure', measure.name
+    assert_redirected_to admin_measures_url
+  end
+
+  test 'should show measure' do
+    get admin_measure_url @measure
+
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_admin_measure_url @measure
+
+    assert_response :success
+  end
+
+  test 'should update measure _en' do
+    patch admin_measure_url @measure, params:
+      { measure:
+          { name: 'New Name' } }
+
+    assert_equal 'Your Measure was successfully updated !', flash[:success]
+    measure = Measure.find @measure.id
+    assert_equal 'New Name', measure.name
+    assert_redirected_to admin_measures_url
+  end
+
+  test 'should destroy measure' do
+    assert_difference('Measure.count', -1) do
+      delete admin_measure_url @measure
+    end
+
+    assert_equal 'Your Measure was successfully deleted !', flash[:success]
+    assert_redirected_to admin_measures_url
+  end
+
+end
---
+++ test/controllers/admin/recipes_controller_test.rb
@@ -0,0 +1,79 @@
+require 'test_helper'
+
+class Admin::RecipesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    default_url_options[:host] = 'en.application.local'
+    @recipe = recipes :tartiflette
+  end
+
+  test 'should get index' do
+    get admin_recipes_url
+
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_admin_recipe_url
+
+    assert_response :success
+  end
+
+  test 'should create recipe _en' do
+    assert_difference('Recipe.count') do
+      post admin_recipes_url, params:
+        { recipe:
+            { name: 'Test Recipe',
+              category_id: categories(:sauce).id,
+              user_id: users(:grimm).id,
+              number_of_people: 5,
+              difficulty: 'hard',
+              price: 'high',
+              image: 'recipes/image.png',
+              user_comment: 'Blah Blah Blah' } }
+    end
+
+    assert_equal 'Your Recipe was successfully created !', flash[:success]
+    recipe = Recipe.last
+    assert_equal 'Test Recipe', recipe.name
+    assert recipe.difficulty_hard?
+    assert recipe.high_price?
+    assert_redirected_to admin_recipes_url
+  end
+
+  test 'should show recipe' do
+    get admin_recipe_url @recipe
+
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_admin_recipe_url @recipe
+
+    assert_response :success
+  end
+
+  test 'should update recipe _en' do
+    patch admin_recipe_url @recipe, params:
+      { recipe:
+          { name: 'New Name',
+            preparation_time: 70,
+            cooking_time: 30 } }
+
+    assert_equal 'Your Recipe was successfully updated !', flash[:success]
+    recipe = Recipe.find @recipe.id
+    assert_equal 'New Name', recipe.name
+    assert_equal 70, recipe.preparation_time
+    assert_equal 30, recipe.cooking_time
+    assert_redirected_to admin_recipes_url
+  end
+
+  test 'should destroy recipe' do
+    assert_difference('Recipe.count', -1) do
+      delete admin_recipe_url @recipe
+    end
+
+    assert_equal 'Your Recipe was successfully deleted !', flash[:success]
+    assert_redirected_to admin_recipes_url
+  end
+
+end
