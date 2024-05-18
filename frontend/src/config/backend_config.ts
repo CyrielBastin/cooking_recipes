a4e6bd5281d301d32e07ba9a21994f71c12f7c2f
Valtyr <cyriel.bastin@zoho.com>
2024-05-18 15:55:31 +0200

    fix typo in url config

--- frontend/src/config/backend_config.ts
+++ frontend/src/config/backend_config.ts
@@ -1,4 +1,4 @@
-export const backend_url = 'htt://localhost:3000'
+export const backend_url = 'http://127.0.0.1:3000'
 export const headers = {
   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0',
   Accept: 'application/json',
